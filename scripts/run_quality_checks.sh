42e3615b7037668c87bb5db5cd079ad9da7120e4
Shahriar Kabir Khan <kb.syl@hotmail.com>
2021-07-29 20:20:09 +0100

    Update run_quality_checks.sh

--- scripts/run_quality_checks.sh
+++ scripts/run_quality_checks.sh
@@ -1,3 +1,7 @@
+python scripts/validate_schema.py --schema tools --json examples/tools/test/tools_test.json
+python scripts/validate_schema.py --schema project --json examples/project/test/project_test.json
+python scripts/validate_schema.py --schema person --json examples/person/test/person_test.json
+
 python scripts/quality_checks.py --schema tools --json examples/tools/test/tools_test.json
 python scripts/quality_checks.py --schema project --json examples/project/test/project_test.json
 python scripts/quality_checks.py --schema person --json examples/person/test/person_test.json
