c69dc7c29d11097ca61fa1f4258979fd845c14f9
Calum Macdonald <calmacx@gmail.com>
2023-09-26 16:24:20 +0100

    fix temporal

--- hdr_schemata/models/GWDM/base/Temporal.py
+++ hdr_schemata/models/GWDM/base/Temporal.py
@@ -11,27 +11,32 @@ class Temporal(BaseModel):
         
     startDate: Optional[Union[date, datetime]] = Field(
         ...,
-        description='.',
+        description='The start of the time period that the dataset provides coverage for',
+        example='12/03/2020',
         title='Start Date',
     )
     endDate: Optional[Union[date, datetime]] = Field(
         None,
-        description='.',
+        description='The end of the time period that the dataset provides coverage for',
+        example='12/03/2020',
         title='End Date',
     )
     timeLag: TimeLag = Field(
         ...,
-        description='t',
+        description='Rypical time-lag between an event and the data for that event appearing in the dataset',
+        example="LESS 1 WEEK",
         title='Time Lag',
     )
         
     accrualPeriodicity: Periodicity = Field(
         ...,
-        description='',
+        description='frequency of distribution release. If a dataset is distributed regularly please choose a distribution release periodicity from the constrained list and indicate the next release date. When the release date becomes historical, a new release date will be calculated based on the publishing periodicity.',
+        example="MONTHLY",
         title='Periodicity',
     )
+    
     distributionReleaseDate: Optional[Union[date, datetime]] = Field(
         None,
-        description='',
+        description='Date of the latest release of the dataset. If this is a regular release i.e. quarterly, or this is a static dataset please complete this alongside Periodicity.',
         title='Release Date',
     )
