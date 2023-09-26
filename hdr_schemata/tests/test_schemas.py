82ce290ee0146d323541d53481a03da3e6a2f58f
Calum Macdonald <calmacx@gmail.com>
2023-09-26 16:41:53 +0100

    update tests

--- hdr_schemata/tests/test_schemas.py
+++ hdr_schemata/tests/test_schemas.py
@@ -8,8 +8,13 @@ def get_metadata(model,version):
     metadata = json.load(open(f'../examples/{model}/{version}/example.json'))
     return metadata
 
+def get_schema(model,version):
+    metadata = json.load(open(f'../models/{model}/{version}/schema.json'))
+    return metadata
+
 class TestHdruk212:
     metadata = get_metadata('HDRUK','2.1.2')
+    json_schema = get_schema('HDRUK','2.1.2')
 
     def test_validation(self):
         assert Hdruk212(**self.metadata) != None
@@ -18,9 +23,12 @@ class TestHdruk212:
         schema = Hdruk212.model_json_schema()
         expected_keys = ['$defs', 'additionalProperties', 'properties', 'required', 'title', 'type']
         assert list(schema.keys()) == expected_keys
+        assert schema == self.json_schema
+        
 
 class TestGwdm10:
     metadata = get_metadata('GWDM','1.0')
+    json_schema = get_schema('GWDM','1.0')
 
     def test_validation(self):
         assert Gwdm10(**self.metadata) != None
@@ -29,3 +37,9 @@ class TestGwdm10:
         schema = Gwdm10.model_json_schema()
         expected_keys = ['$defs', 'additionalProperties', 'properties', 'required', 'title', 'type']
         assert list(schema.keys()) == expected_keys
+
+    def test_json_schema(self):
+        schema = Gwdm10.model_json_schema()
+        expected_keys = ['$defs', 'additionalProperties', 'properties', 'required', 'title', 'type']
+        assert list(schema.keys()) == expected_keys
+        assert schema == self.json_schema
