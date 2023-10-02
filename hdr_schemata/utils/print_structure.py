ee79efd540d5e6cc6c02405b00494ca31bd79e69
Calum Macdonald <calmacx@gmail.com>
2023-10-02 13:59:22 +0100

    update available

--- hdr_schemata/utils/print_structure.py
+++ hdr_schemata/utils/print_structure.py
@@ -2,10 +2,10 @@ import glob
 import json
 
 structure = {}
-for schema in glob.glob('metadata/**/schema.json',recursive=True):
+for schema in glob.glob('hdr_schemata/models/**/schema.json',recursive=True):
     items = schema.split('/')
-    model = items[1]
-    version = items[2]
+    model = items[2]
+    version = items[3]
     if model not in structure:
         structure[model] = []
     structure[model].append(version)
