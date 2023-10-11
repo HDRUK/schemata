4b652ba95261844dbf436e4f2cb4708f998a84e5
Calum Macdonald <calmacx@gmail.com>
2023-10-11 16:58:19 +0100

    change access rights

--- hdr_schemata/models/HDRUK/test.py
+++ hdr_schemata/models/HDRUK/test.py
@@ -2,13 +2,5 @@ from pydantic import ValidationError
 import v2_1_2
 import json
 
-
-metadata = json.load(open('2.1.2/example.json'))
-
-try:
-    v2_1_2.Hdruk212(**metadata)
-    v2_1_2.Hdruk212.save_schema()
-except ValidationError as e:
-    print(e)
-
+v2_1_2.Hdruk212.save_schema()
 
