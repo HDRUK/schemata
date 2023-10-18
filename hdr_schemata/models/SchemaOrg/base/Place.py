e7533575286708c563f247f607269267866c8f14
Calum Macdonald <calmacx@gmail.com>
2023-10-18 13:55:38 +0100

    include eligable region

--- hdr_schemata/models/SchemaOrg/base/Place.py
+++ hdr_schemata/models/SchemaOrg/base/Place.py
@@ -16,3 +16,8 @@ class Place(Thing):
         None,
         description='GeoCoordinates of the place'
     )
+
+    eligibleRegion: Optional[Text] = Field(
+        None,
+        description = 'The ISO 3166-1 (ISO 3166-1 alpha-2) or ISO 3166-2 code, the place, or the GeoShape for the geo-political region(s) for which the offer or delivery charge specification is valid.'
+    )
