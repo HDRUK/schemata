4e3531d62d147d2e3b6d26a782970c0ea555d7b9
Calum Macdonald <calmacx@gmail.com>
2023-10-26 11:42:04 +0100

    update for testing

--- setup.py
+++ setup.py
@@ -20,7 +20,8 @@ setuptools.setup(
     url="https://github.com/HDRUK/schemata-2",
     packages=setuptools.find_packages(),
     install_requires=[
-        "pydantic[email]==2.0.3",
+        #"pydantic[email]==2.0.3",
+        "pydantic==2.4.2",
         "pytest==7.4.2"
     ],
     classifiers=[
