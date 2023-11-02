60f221a267c3a1874a3d39cddcc1dbed34a69f6b
Calum Macdonald <calmacx@gmail.com>
2023-11-02 08:57:25 +0000

    add model v1.1

--- hdr_schemata/models/GWDM/create_json_schema.py
+++ hdr_schemata/models/GWDM/create_json_schema.py
@@ -1,8 +1,10 @@
 from pydantic import ValidationError
 from v1_0 import Gwdm10
+from v1_1 import Gwdm11
 import json
 
 Gwdm10.save_schema('1.0/schema.json')
-Gwdm10.save_schema('latest/schema.json')
+Gwdm11.save_schema('1.1/schema.json')
+Gwdm11.save_schema('latest/schema.json')
 
 
