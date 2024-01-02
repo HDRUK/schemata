13ef5e06026803e9f53599bd7502c4d249dd25fb
Calum Macdonald <calmacx@gmail.com>
2024-01-02 10:49:01 +0000

    remove latest as it messes up traser

--- hdr_schemata/models/GWDM/create_json_schema.py
+++ hdr_schemata/models/GWDM/create_json_schema.py
@@ -2,7 +2,4 @@ from pydantic import ValidationError
 from v1_0 import Gwdm10
 import json
 
-Gwdm10.save_schema('1.0/schema.json')
-Gwdm10.save_schema('latest/schema.json')
-
-
+Gwdm10.save_schema("1.0/schema.json")
