1be6f854c5d8e2bc815d9e6ad4c229d293928e83
Calum Macdonald <calmacx@gmail.com>
2024-01-08 15:13:37 +0000

    didnt register changes

--- hdr_schemata/models/HDRUK/v2_1_3/__init__.py
+++ hdr_schemata/models/HDRUK/v2_1_3/__init__.py
@@ -1,8 +1,17 @@
 from hdr_schemata.models.HDRUK.v2_1_2 import Hdruk212
 import json
+from typing import Optional
+from pydantic import Field
+from .Provenance import Provenance
 
 
 class Hdruk213(Hdruk212):
+    provenance: Optional[Provenance] = Field(
+        None,
+        description="Provenance information allows researchers to understand data within the context of its origins and can be an indicator of quality, authenticity and timeliness.",
+        title="Provenance",
+    )
+
     @classmethod
     def save_schema(cls, location="./2.1.3/schema.json"):
         with open(location, "w") as f:
