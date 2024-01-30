707e830abbbe2d6adb552dc81885176b3f592146
Calum Macdonald <calmacx@gmail.com>
2024-01-30 14:03:39 +0000

    fix lengths for version control

--- hdr_schemata/models/GWDM/v1_1/Required.py
+++ hdr_schemata/models/GWDM/v1_1/Required.py
@@ -2,12 +2,11 @@ from hdr_schemata.models.GWDM.v1_0 import Required as BaseRequired
 from typing import Optional
 from pydantic import Field, constr
 
-class Required(BaseRequired):
-    
-    version: constr(min_length=2,max_length=100) = Field(
-        ...,
-        description='Dataset metadata version',
-        examples=['1.1.0'],
-        title='Dataset Version',
-    )
 
+class Required(BaseRequired):
+    version: constr(min_length=3, max_length=4) = Field(
+        ...,
+        description="Dataset metadata version",
+        examples=["1.1.0"],
+        title="Dataset Version",
+    )
