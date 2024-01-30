9fba122e27c2af1f0b48e4285fe5c764499d8031
Calum Macdonald <calmacx@gmail.com>
2024-01-30 11:51:41 +0000

    adding in population size

--- hdr_schemata/models/GWDM/v1_1/Summary.py
+++ hdr_schemata/models/GWDM/v1_1/Summary.py
@@ -1,14 +1,20 @@
 from hdr_schemata.models.GWDM.v1_0 import Summary as BaseSummary
-from pydantic import Field 
+from pydantic import Field
 from typing import Optional
 from .Organisation import Organisation
 
-class Summary(BaseSummary):
 
-    #switch publisher to be an Organisation
+class Summary(BaseSummary):
+    # switch publisher to be an Organisation
     publisher: Optional[Organisation] = Field(
         ...,
         description="Link to details about the publisher of this dataset",
-        title='Publisher',
+        title="Publisher",
     )
 
+    # new field for a summary of the population size
+    populationSize: Optional[int] = Field(
+        ...,
+        description="Summary population size of the cohort",
+        title="Population size",
+    )
