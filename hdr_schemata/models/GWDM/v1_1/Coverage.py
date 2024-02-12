6f63c6d61b24858f9a99870e7accf5d48cfbff5d
Calum Macdonald <calmacx@gmail.com>
2024-02-12 12:43:29 +0000

    coverage is too strict for the GWDM 1.1

--- hdr_schemata/models/GWDM/v1_1/Coverage.py
+++ hdr_schemata/models/GWDM/v1_1/Coverage.py
@@ -2,128 +2,45 @@ from hdr_schemata.models import remove_fields_from_cls
 from hdr_schemata.models.GWDM.v1_0 import Coverage as BaseCoverage
 import re
 from typing import Optional, List
-from pydantic import Field, RootModel, constr
-
-
-def get_pattern(allowed_phrases):
-    return (
-        r"\b(?:"
-        + "|".join(allowed_phrases)
-        + r")(?:,(?:"
-        + "|".join(allowed_phrases)
-        + r"))*\b"
-    )
-
-
-class Anthropometric(RootModel):
-    root: Optional[
-        constr(
-            pattern=get_pattern(
-                [
-                    "Blood Pressure",
-                    "Hip Circumference",
-                    "Height",
-                    "Waist Circumference",
-                    "Weight",
-                ]
-            )
-        )
-    ]
-
-
-class BiologicalSamples(RootModel):
-    root: Optional[constr(pattern=get_pattern(["Blood", "Other", "Urine", "Saliva"]))]
-
-
-class Physical(RootModel):
-    root: Optional[
-        constr(
-            pattern=get_pattern(
-                [
-                    "Respiratory",
-                    "Vision",
-                    "Hearing",
-                    "Musculoskeletal",
-                    "Cardiovascular",
-                    "Reproductive",
-                ]
-            )
-        )
-    ]
-
-
-class Psychological(RootModel):
-    root: Optional[constr(pattern=get_pattern(["Cognitive Function", "Mental Health"]))]
-
-
-class Lifestyles(RootModel):
-    root: Optional[
-        constr(
-            pattern=get_pattern(
-                ["Smoking", "Dietary Habits", "Physical Activity", "Alcohol"]
-            )
-        )
-    ]
-
-
-class Gender(RootModel):
-    root: Optional[constr(pattern=get_pattern(["Male", "Female", "Other"]))]
-
-
-class SocioEconomic(RootModel):
-    root: Optional[
-        constr(
-            pattern=get_pattern(
-                [
-                    "Finances",
-                    "Family Circumstances",
-                    "Housing",
-                    "Education",
-                    "Marital Status",
-                    "Occupation",
-                    "Ethnic Group",
-                    "Social Support",
-                ]
-            )
-        )
-    ]
+from pydantic import Field
+from hdr_schemata.definitions.HDRUK import CommaSeparatedValues
 
 
 class Coverage(BaseCoverage):
     class Config:
         extra = "forbid"
 
-    gender: Optional[Gender] = Field(
+    gender: Optional[CommaSeparatedValues] = Field(
         None, title="Gender", description="Male, Female, Other"
     )
 
-    biologicalsamples: Optional[BiologicalSamples] = Field(
+    biologicalsamples: Optional[CommaSeparatedValues] = Field(
         None, title="Biological Samples", description="Blood, Saliva, Urine, Other"
     )
 
-    psychological: Optional[Psychological] = Field(
+    psychological: Optional[CommaSeparatedValues] = Field(
         None, title="Psychological", description="Mental health, Cognitive function"
     )
 
-    physical: Optional[Physical] = Field(
+    physical: Optional[CommaSeparatedValues] = Field(
         None,
         title="Physical",
         description="Cardiovascular, Respiratory, Musculoskeletal, Hearing and Vision, Reproductive",
     )
 
-    anthropometric: Optional[Anthropometric] = Field(
+    anthropometric: Optional[CommaSeparatedValues] = Field(
         None,
         title="Anthropometric",
         description="Height, Weight, Waist circumference, Hip circumference, Blood pressure",
     )
 
-    lifestyle: Optional[Lifestyles] = Field(
+    lifestyle: Optional[CommaSeparatedValues] = Field(
         None,
         title="Lifestyle",
         description="Cohort lifestyle habits: Smoking, Physical activity, Dietary habits, Alcohol",
     )
 
-    socioeconomic: Optional[SocioEconomic] = Field(
+    socioeconomic: Optional[CommaSeparatedValues] = Field(
         None,
         title="Socio-economic",
         description="Occupation, Family circumstances, Housing, Education, Ethnic group, Marital status, Social support",
