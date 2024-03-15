2d96ff6eae7d881fb6e33871f817b631141d3b18
Calum Macdonald <calmacx@gmail.com>
2024-03-15 09:49:23 +0000

    updating some change logs and descriptions

--- hdr_schemata/models/HDRUK/v2_1_2/Documentation.py
+++ hdr_schemata/models/HDRUK/v2_1_2/Documentation.py
@@ -5,29 +5,31 @@ from hdr_schemata.definitions.HDRUK import *
 
 class Documentation(BaseModel):
     class Config:
-        extra = 'forbid'
+        extra = "forbid"
 
     description: Optional[Description] = Field(
-        None, description='A free-text description of the record.', title='Description'
+        None,
+        description="A free-text description of the dataset. Gateway Feature: Keywords and text may be extracted out of the description and index for search",
+        title="Description",
     )
-    
+
     associatedMedia: Optional[
         Union[Optional[CommaSeparatedValues], List[Optional[Url]]]
     ] = Field(
         None,
-        description='Please provide any media associated with the Gateway Organisation using a valid URI for the content. This is an opportunity to provide additional context that could be useful for researchers wanting to understand more about the dataset and its relevance to their research question. The following formats will be accepted .jpg, .png or .svg, .pdf, .xslx or .docx. Note: media asset can be hosted by the organisation or uploaded using the onboarding portal.',
-        examples=['PDF Document that describes study protocol'],
-        title='Associated Media',
+        description="Please provide any media associated with the Gateway Organisation using a valid URI for the content. This is an opportunity to provide additional context that could be useful for researchers wanting to understand more about the dataset and its relevance to their research question. The following formats will be accepted .jpg, .png or .svg, .pdf, .xslx or .docx. Note: media asset can be hosted by the organisation or uploaded using the onboarding portal.",
+        examples=["PDF Document that describes study protocol"],
+        title="Associated Media",
     )
-    
+
     isPartOf: Optional[
         Union[
             Optional[CommaSeparatedValues],
             List[Union[Optional[Url], OneHundredFiftyCharacters, IsPartOfEnum]],
         ]
     ] = Field(
-        'NOT APPLICABLE',
-        description='Please complete only if the dataset is part of a group or family',
-        examples=['Hospital Episodes Statistics datasets (A&E, APC, OP, AC MSDS).'],
-        title='Group',
+        "NOT APPLICABLE",
+        description="Please complete only if the dataset is part of a group or family",
+        examples=["Hospital Episodes Statistics datasets (A&E, APC, OP, AC MSDS)."],
+        title="Group",
     )
