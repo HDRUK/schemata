1ceef6e442575c25f9c389d00bd6330de48d244c
Calum Macdonald <calmacx@gmail.com>
2024-03-21 14:13:50 +0000

    separating out testing

---
+++ hdr_schemata/tests/test_gwdm.py
@@ -0,0 +1,37 @@
+from pydantic import ValidationError
+import json
+
+from hdr_schemata.models.GWDM import Gwdm10
+
+
+def get_metadata(model, version):
+    metadata = json.load(open(f"../examples/{model}/{version}/example.json"))
+    return metadata
+
+
+def get_schema(model, version):
+    metadata = json.load(open(f"../models/{model}/{version}/schema.json"))
+    return metadata
+
+
+class TestGwdm10:
+    def __init__(self):
+        self.metadata = get_metadata("GWDM", "1.0")
+        self.json_schema = get_schema("GWDM", "1.0")
+
+    def test_validation(self):
+        assert Gwdm10(**self.metadata) != None
+
+    def test_json_schema(self):
+        schema = Gwdm10.model_json_schema()
+        expected_keys = [
+            "$defs",
+            "additionalProperties",
+            "properties",
+            "required",
+            "title",
+            "type",
+        ]
+
+        assert list(schema.keys()) == expected_keys
+        assert schema == self.json_schema
--- hdr_schemata/tests/test_schemaOrg.py
+++ hdr_schemata/tests/test_schemaOrg.py
@@ -1,8 +1,6 @@
 from pydantic import ValidationError
 import json
 
-from hdr_schemata.models.HDRUK import Hdruk212
-from hdr_schemata.models.GWDM import Gwdm10
 from hdr_schemata.models.SchemaOrg import GoogleRecommendedDataset
 from hdr_schemata.models.SchemaOrg import BioSchema
 
@@ -15,49 +13,6 @@ def get_metadata(model, version):
 def get_schema(model, version):
     metadata = json.load(open(f"../models/{model}/{version}/schema.json"))
     return metadata
-
-
-class TestHdruk212:
-    metadata = get_metadata("HDRUK", "2.1.2")
-    json_schema = get_schema("HDRUK", "2.1.2")
-
-    def test_validation(self):
-        assert Hdruk212(**self.metadata) != None
-
-    def test_json_schema(self):
-        schema = Hdruk212.model_json_schema()
-        expected_keys = [
-            "$defs",
-            "additionalProperties",
-            "properties",
-            "required",
-            "title",
-            "type",
-        ]
-        assert list(schema.keys()) == expected_keys
-        assert schema == self.json_schema
-
-
-class TestGwdm10:
-    metadata = get_metadata("GWDM", "1.0")
-    json_schema = get_schema("GWDM", "1.0")
-
-    def test_validation(self):
-        assert Gwdm10(**self.metadata) != None
-
-    def test_json_schema(self):
-        schema = Gwdm10.model_json_schema()
-        expected_keys = [
-            "$defs",
-            "additionalProperties",
-            "properties",
-            "required",
-            "title",
-            "type",
-        ]
-
-        assert list(schema.keys()) == expected_keys
-        assert schema == self.json_schema
 
 
 class TestGoogleRecommended:
