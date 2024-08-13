548e91ba44f900eb9b4825e22b662a00b69d87c6
Calum Macdonald <calmacx@gmail.com>
2024-08-13 08:05:26 +0100

    fix available.json

--- hdr_schemata/utils/print_structure.py
+++ hdr_schemata/utils/print_structure.py
@@ -1,6 +1,15 @@
 import glob
 import json
 
+def sort_versions(versions):
+    def version_key(version):
+        try:
+            return tuple(map(int, (version.split("."))))
+        except ValueError:
+            return (version.lower(),)
+    
+    return sorted(versions, key=version_key)
+
 structure = {}
 for schema in glob.glob("hdr_schemata/models/**/schema.json", recursive=True):
     items = schema.split("/")
@@ -11,5 +20,8 @@ for schema in glob.glob("hdr_schemata/models/**/schema.json", recursive=True):
     if model not in structure:
         structure[model] = []
     structure[model].append(version)
+
+for model in structure:
+    structure[model] = sort_versions(structure[model])
 
 json.dump(structure, open("available.json", "w"), indent=6)
