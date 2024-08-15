27f52ee530682b03dc7c693815e07aeebeebaff2
Calum Macdonald <calmacx@gmail.com>
2024-08-15 12:21:05 +0100

    fix format and standards

--- hdr_schemata/models/HDRUK/v3_0_0/FormatAndStandards.py
+++ hdr_schemata/models/HDRUK/v3_0_0/FormatAndStandards.py
@@ -12,19 +12,23 @@ class FormatAndStandards(BaseModel):
         extra = "forbid"
 
     vocabularyEncodingScheme: List[ControlledVocabulary] = Field(
-        ..., 
-        **an.vocabularyEncodingScheme.__dict__, 
+        ...,
+        **an.vocabularyEncodingScheme.__dict__,
         json_schema_extra={"guidance": an.vocabularyEncodingScheme.guidance}
     )
 
     conformsTo: List[StandardisedDataModels] = Field(
-        ..., **an.conformsTo.__dict__, json_schema_extra={"guidance": an.conformsTo.guidance}
+        ...,
+        **an.conformsTo.__dict__,
+        json_schema_extra={"guidance": an.conformsTo.guidance}
     )
 
-    language: Union[Optional[CommaSeparatedValues], List[Language]] = Field(
-        ..., **an.language.__dict__, json_schema_extra={"guidance": an.language.guidance}
+    language: List[Language] = Field(
+        ...,
+        **an.language.__dict__,
+        json_schema_extra={"guidance": an.language.guidance}
     )
 
-    format: Union[Optional[CommaSeparatedValues], List[Format]] = Field(
+    format: List[Format] = Field(
         ..., **an.format.__dict__, json_schema_extra={"guidance": an.format.guidance}
     )
