0e8fe3bf5a4fcb179accf29ebccfaf57e9d20e37
Calum Macdonald <calmacx@gmail.com>
2024-09-20 11:57:12 +0100

    fix gwmd datasettype

--- hdr_schemata/models/HDRUK/v3_0_0/Access.py
+++ hdr_schemata/models/HDRUK/v3_0_0/Access.py
@@ -12,36 +12,49 @@ class Access(BaseModel):
         extra = "forbid"
 
     accessRights: LongDescription = Field(
-        ..., **an.accessRights.__dict__, json_schema_extra={"guidance": an.accessRights.guidance}
+        ...,
+        **an.accessRights.__dict__,
+        json_schema_extra={"guidance": an.accessRights.guidance}
     )
 
     accessServiceCategory: Optional[AccessService] = Field(
-        None, 
-        **an.accessServiceCategory.__dict__, 
+        None,
+        **an.accessServiceCategory.__dict__,
         json_schema_extra={"guidance": an.accessServiceCategory.guidance}
     )
 
     accessService: Optional[LongDescription] = Field(
-        None, **an.accessService.__dict__, json_schema_extra={"guidance": an.accessService.guidance}
+        None,
+        **an.accessService.__dict__,
+        json_schema_extra={"guidance": an.accessService.guidance}
     )
-    
+
     accessRequestCost: Optional[LongDescription] = Field(
-        None, **an.accessRequestCost.__dict__, json_schema_extra={"guidance": an.accessRequestCost.guidance}
+        None,
+        **an.accessRequestCost.__dict__,
+        json_schema_extra={"guidance": an.accessRequestCost.guidance}
     )
-    
+
     deliveryLeadTime: Optional[DeliveryLeadTimeV2] = Field(
-        None, **an.deliveryLeadTime.__dict__, json_schema_extra={"guidance": an.deliveryLeadTime.guidance}
+        None,
+        **an.deliveryLeadTime.__dict__,
+        json_schema_extra={"guidance": an.deliveryLeadTime.guidance}
     )
 
     jurisdiction: Optional[List[Isocountrycode]] = Field(
-        None, **an.jurisdiction.__dict__, json_schema_extra={"guidance": an.jurisdiction.guidance}
+        None,
+        **an.jurisdiction.__dict__,
+        json_schema_extra={"guidance": an.jurisdiction.guidance}
     )
-    
+
     dataController: Optional[LongDescription] = Field(
-        None, **an.dataController.__dict__, json_schema_extra={"guidance": an.dataController.guidance}
+        None,
+        **an.dataController.__dict__,
+        json_schema_extra={"guidance": an.dataController.guidance}
     )
-    
+
     dataProcessor: Optional[LongDescription] = Field(
-        None, **an.dataProcessor.__dict__, json_schema_extra={"guidance": an.dataProcessor.guidance}
+        None,
+        **an.dataProcessor.__dict__,
+        json_schema_extra={"guidance": an.dataProcessor.guidance}
     )
-    
